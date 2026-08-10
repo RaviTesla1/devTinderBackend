8368bc4b8430cc4dacc167d4d258c89129b111dc
RaviTesla1 <ravibatalvi55@gmail.com>
2026-08-10 14:23:43 +0530

    new route explored

--- src/app.js
+++ src/app.js
@@ -2,21 +2,35 @@ const express = require('express');
 
 const app  = express();
 
-app.use("/hello/2", (req, res) => {
-  res.send("Abra ka dabra")
+app.get("/user",(req,res) =>{
+  res.send({"firstName": "ravi" , "lastname": "kumar"})
 })
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello")
+app.post("/user",(req,res) =>{
+  console.log("Save data to the database");
+  res.send("Data successfully saved to the database")
+})
+
+app.delete("/user",(req,res)=> {
+  res.send("Data successfully deleted")
 })
 
 app.use("/test", (req, res) => {
   res.send("Hello world from the server")
 })
 
-app.use("/", (req, res) => {
-  res.send("Namaste from from the Dashboard")
-})
+// app.use("/", (req, res) => {
+//   res.send("Namaste from from the Dashboard")
+// })
+// app.use("/hello/2", (req, res) => {
+//   res.send("Abra ka dabra")
+// })
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello hello")
+// })
+
+
 app.listen(3000,() => {
   console.log("Server is succuessfully running on port 3000")
 })
