a4a4e13634a8ae66311be101985a88657b474676
RaviTesla1 <ravibatalvi55@gmail.com>
2026-08-11 14:50:33 +0530

    Why we need Middleware

--- src/app.js
+++ src/app.js
@@ -2,39 +2,32 @@ const express = require('express');
 
 const app  = express();
 
-app.get("/user",
-  [(req,res,next) => {
-  console.log("Handling user 1");
-  
-  next();
-},
- (req,res,next) => {  
-  console.log("Handling user 2");
-  
-  // res.send("User 2 created succesfully")
-    next();
 
-},
- (req,res,next) => {  
+app.get("/admin/getAllUser",
+  (req,res,next) => {
+    
+    const token = "abc"
+    const isAdminAuthorized = token === "abcd"
+    if(!isAdminAuthorized){
+      res.status(401).send("Unauthorized");   
+    }
+    else res.send("All user list")
+   
+  })
+
+  app.get("/admin/deleteUser",
+  (req,res,next) => {
+    
+    const token = "abc"
+    const isAdminAuthorized = token === "abc"
+    if(!isAdminAuthorized){
+      res.status(401).send("Unauthorized");   
+    }
+    else res.send("User deleted succesfully")
+   
+  })
   
-  console.log("Handling user 3");
-      next();
-
-  
-  // res.send("User 3 created succesfully")
-},
- (req,res,next) => {  
-  
-  console.log("Handling user 4");
-  next();
-  
-  res.send("User 4 created succesfully")
-}]
-)
-
-
-
-
+ 
 app.listen(3000,() => {
   console.log("Server is succesfully running on port 3000")
 })
