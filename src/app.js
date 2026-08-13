8ac7aa0c4863a2f5d0a8825e82ca9de18af332cf
RaviTesla1 <ravibatalvi55@gmail.com>
2026-08-13 12:13:09 +0530

    wildcard error-handling

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const {adminAuth,userAuth} = require("./middlewares/auth")
-
 const app = express();
 
-app.use("/admin",adminAuth)
-app.use("/use",userAuth)
+app.use("/",(err,req,res,next) => {
+  if(err){
+    res.status(500).send("Something went wrong")
+  }
+})
+ 
 
-
-
-app.get("/admin/getAllUser", (req, res, next) => {
-  res.send("All user list");
-});  
-
-app.get("/user", (req, res) => {
-  res.send("All user list");
+app.get("/getUserData", (req, res) => {
+  // try{
+    throw new Error("njkfjdjk");
+    res.send("User Data Sent");
+  // }
+ /*  catch(err){
+    res.status(500).send("some error found contact support team");
+  } */
+  
 });
+app.use("/",(err,req,res,next) => {
+  if(err){
+    res.status(500).send("Unauthorized request")
+  }
+})
 
-app.get("/admin/deleteUser", (req, res, next) => {
-  const token = "abc";
-  const isAdminAuthorized = token === "abc";
-  if (!isAdminAuthorized) {
-    res.status(401).send("Unauthorized");
-  } else res.send("User deleted succesfully");
-});
 
 app.listen(3000, () => {
   console.log("Server is succesfully running on port 3000");
