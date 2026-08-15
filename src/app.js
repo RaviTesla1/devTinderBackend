0516a6357b4173a9e776ef02f1dde4049783e001
RaviTesla1 <ravibatalvi55@gmail.com>
2026-08-15 10:59:12 +0530

    scheme Validations

--- src/app.js
+++ src/app.js
@@ -18,6 +18,66 @@ app.post("/signup",async (req,res)=>{
     res.status(400).send("Error saving the user :"  + err.message)
   }
 })
+// Get user by email
+app.get("/user",async (req,res) =>{
+  console.log("Email =======>>>>>",req.body.email)
+  const email = req.body.email;
+
+   try{
+    const users = await User.findOne({email:email });
+
+    if(!users){
+      res.status(404).send("User not found")
+    }else res.send(users)
+  }catch(err){
+    res.status(400).send("User not found :"  + err.message)
+  }
+
+})
+// Feed Api - GET / feed - get all the users from the database
+app.get("/feed",async(req,res) =>{
+  try {
+    const users = await User.find({});
+    res.send(users)
+  }catch(err){
+    res.status(400).send("Something went wrong")
+  }
+
+})
+
+app.delete("/user", async (req,res)=>{
+  console.log("user ======>>>>>",req.body.userId)
+  const userId = await req.body.userId;
+  try{
+    // await User.findByIdAndDelete(userId)
+    await User.findByIdAndDelete({_id:userId})
+    res.send("User deleted successfully")
+  }catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+
+// USER UPDATE :
+app.patch("/user", async (req, res) =>{
+  const userId =  req.body.userId;
+  const data =  req.body;
+/*   console.log("userId=====>",userId)
+  console.log("req.body.data ======>>>>",req.body) */
+
+  try{
+    const user = await User.findByIdAndUpdate(userId, data,{
+      returnDocument :"after",
+      runValidators : true
+    });
+    console.log("user---->>>>",user)
+    res.send("User updated successfully");
+  }
+  catch(err){
+    res.status(400).send("UPDATE FAILDED :" + err.message)
+  }
+})
+
+
 
 connectDB()
    .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,52 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName : {
-    type : String
+  firstName: {
+    type: String,
+    required: true,
+    minLength : 4,
+    maxLength : 50
   },
-  lastName : {
-    type : String
+  lastName: {
+    type: String,
   },
-  email : {
-    type : String
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase : true,
+    trim : true
   },
-  password : {
-    type : String
+  password: {
+    type: String,
+    required: true,
   },
-  age : {
-    type : Number
+  age: {
+    type: Number,
+    min : 18
   },
-  gender : {
-    type : String
-  }
-
-})
+  gender: {
+    type: String,
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new Error("Gender Data is not valid")
+      }
+    }
+  },
+  photoURl: {
+    type: String,
+    default: "https://geographyandyou.com/images/user-profile.png",
+  },
+  about: {
+    type: String,
+    default: "This is a default value of user",
+  },
+  skills: {
+    type: [String],
+  },
+},
+ { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
